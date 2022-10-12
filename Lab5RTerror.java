ca7b127e194df8f194f9ef10d153e7dde851cc9e
EmmanuelC05 <113859794+EmmanuelC05@users.noreply.github.com>
2022-10-11 22:51:08 -0500

    Update Lab5RTerror.java

--- Lab5RTerror.java
+++ Lab5RTerror.java
@@ -13,7 +13,7 @@ public class Lab5RTerror {
     public static void main(String[] args) 
     {
         int arr[] = new int[5];
-        
+        //I assigned the value 250 to index 2 inorder to make the error work.
         arr[2] = 250;
         
         System.out.println("Value assigned");
